41383c87c91032c8c609d9d72b1f8257f385f204
Michael Mican <michael.mican@stud.th-deg.de>
2019-04-08 11:59:50 +0200

    Add fundamental classes

---
+++ src/GameTable.java
@@ -0,0 +1,10 @@
+public class GameTable {
+
+    private GameView gameView;
+    private Ball[] ball;
+    private Player[] player;
+    private Entity[] entities;
+
+    public GameTable() {
+    }
+}
--- src/GameView.java
+++ src/GameView.java
@@ -1,5 +1,3 @@
-package marsinvasion;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
