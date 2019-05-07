6dbff1290b8abd559617374944c3eaaf97319957
Michael Mican <michael.mican@stud.th-deg.de>
2019-05-07 15:41:29 +0200

    Change class structure

--- src/asciipinball/Program.java
+++ src/asciipinball/Program.java
@@ -1,16 +1,14 @@
 package asciipinball;
 
-import asciipinball.GameTable;
-
 public class Program {
     public static void main (String[] args){
 
-        GameTable gameTable = new GameTable(29, 29);
-        gameTable.drawAll();
+        PinballGame pinballGame = new PinballGame(29, 29);
+        pinballGame.drawAll();
 
         while(true){
-            gameTable.simulateTick();
-            gameTable.drawAll();
+            pinballGame.simulateTick();
+            pinballGame.drawAll();
             try {
                 Thread.sleep(200);
             }catch (Exception e){
