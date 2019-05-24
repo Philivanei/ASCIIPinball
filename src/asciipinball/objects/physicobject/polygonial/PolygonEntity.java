2fba892051192a37a7ed8f12f7f54aa2038a3781
Michael Mican <michael.mican@stud.th-deg.de>
2019-05-24 12:39:40 +0200

    change isColided

--- src/asciipinball/objects/physicobject/polygonial/PolygonEntity.java
+++ src/asciipinball/objects/physicobject/polygonial/PolygonEntity.java
@@ -19,28 +19,29 @@ public abstract class PolygonEntity extends PhysicEntity {
         float minDistance = Float.MAX_VALUE;
 
         for(int i = 0; i< lines.length; i++){
-            if(Line2D.linesIntersect(ball.getPositionX(),ball.getPositionY(),
-                    ball.getFuturePositionX(), ball.getFuturePositionY(),
-                    lines[i].getX1(), lines[i].getY1(),
-                    lines[i].getX2(), lines[i].getY2())){ //This checks if the lineSegments intersect
 
-                float mBall = (ball.getFuturePositionY() - ball.getPositionY()) / (ball.getFuturePositionX() - ball.getPositionX());
-                float bBall = ball.getPositionY() - mBall * ball.getPositionX();
-                float mLine = (lines[i].getY2() - lines[i].getY1()) / (lines[i].getX2() - lines[i].getX1());
-                float bLine = lines[i].getY1() - mLine * lines[i].getX1();
+            float mLine = (lines[i].getY2() - lines[i].getY1()) / (lines[i].getX2() - lines[i].getX1());
+            float bLine = lines[i].getY1() - mLine * lines[i].getX1();
+            float mVertical = -1/mLine;
+            float bVertical = ball.getPositionY() - mVertical * ball.getPositionX();
 
-                float x = (bLine - bBall) / (mBall - mLine);
-                float y = mBall * x + mBall;
+            float x = (bLine - bVertical) / (bVertical - mLine);
+            float y = mVertical * x + bVertical;
 
-                float distancePosCollision = (float) Math.sqrt((x-ball.getPositionX()) * (x-ball.getPositionX()) + (y-ball.getPositionY()) * (y-ball.getPositionY()));
+            //TODO evaluierung ob Lot fußpunkt auf strecke ist oder nicht Wenn nicht - abstand zu Strecken Außenpunkten berechnen
 
-                if(distancePosCollision < minDistance){
-                    minDistance = distancePosCollision;
+            //calc distance Ball to lotfußpunkt
+            float distanceToBall = (float) Math.sqrt((x-ball.getPositionX()) * (x-ball.getPositionX()) + (y-ball.getPositionY()) * (y-ball.getPositionY()));
+
+
+
+            if(distanceToBall < ball.getRadius()) {
+                collisionDetected = true;
+                if(distanceToBall < minDistance){
+                    minDistance = distanceToBall;
                     collisionPositionX = x;
                     collisionPositionY = y;
                 }
-
-                collisionDetected = true;
             }
         }
 
