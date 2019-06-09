f8081faa2b6b035ada1fa27b5d45378a835a4a26
Philipp Muhr <p.m33@gmx.de>
2019-06-09 16:44:09 +0200

    change Menu to StartScreen;implement StartScreen (WIP)

---
+++ src/asciipinball/graphics/StartScreen.java
@@ -0,0 +1,37 @@
+package asciipinball.graphics;
+
+
+import asciipinball.corelogic.players.PlayerManager;
+
+public class StartScreen {
+    private PlayerManager player;
+    private boolean isMenuActive;
+    private int playerNumber;
+    private byte counter;
+//key for launchControl
+    public StartScreen() {
+        playerNumber = 0;
+        isMenuActive = true;
+    }
+
+    private void checkPlayer() {
+        while (playerNumber == 0) {
+
+        }
+    }
+
+    public void enterDownPressed(){
+
+    }
+
+
+    public void arrowDownPressed(){
+
+    }
+
+
+    public void arrowUpPressed(){
+
+    }
+
+}
