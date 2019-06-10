97a75218193aee686fad8ff65290accc4f7d840e
Philipp Muhr <p.m33@gmx.de>
2019-06-10 10:13:46 +0200

    add FontBig

---
+++ src/asciipinball/fonts/FontBig.java
@@ -0,0 +1,372 @@
+package asciipinball.fonts;
+
+public class FontBig extends AsciiStringContainer {
+    @Override
+    public String getA() {
+        return "     /\\        \n" +
+                "    /  \\       \n" +
+                "   / /\\ \\      \n" +
+                "  / ____ \\     \n" +
+                " /_/    \\_\\    ";
+    }
+
+    @Override
+    public String getB() {
+        return "  ____         \n" +
+                " |  _ \\        \n" +
+                " | |_) |       \n" +
+                " |  _ <        \n" +
+                " | |_) |       \n" +
+                " |____/        "
+    }
+
+    @Override
+    public String getC() {
+        return "   _____       \n" +
+                "  / ____|      \n" +
+                " | |           \n" +
+                " | |           \n" +
+                " | |____       \n" +
+                "  \\_____|      ";
+    }
+
+    @Override
+    public String getD() {
+        return "  _____        \n" +
+                " |  __ \\       \n" +
+                " | |  | |      \n" +
+                " | |  | |      \n" +
+                " | |__| |      \n" +
+                " |_____/       ";
+    }
+
+    @Override
+    public String getE() {
+        return "  ______       \n" +
+                " |  ____|      \n" +
+                " | |__         \n" +
+                " |  __|        \n" +
+                " | |____       \n" +
+                " |______|      ";
+    }
+
+    @Override
+    public String getF() {
+        return "  ______       \n" +
+                " |  ____|      \n" +
+                " | |__         \n" +
+                " |  __|        \n" +
+                " | |           \n" +
+                " |_|           ";
+    }
+
+    @Override
+    public String getG() {
+        return "   _____       \n" +
+                "  / ____|      \n" +
+                " | |  __       \n" +
+                " | | |_ |      \n" +
+                " | |__| |      \n" +
+                "  \\_____|      ";
+    }
+
+    @Override
+    public String getH() {
+        return "  _    _       \n" +
+                " | |  | |      \n" +
+                " | |__| |      \n" +
+                " |  __  |      \n" +
+                " | |  | |      \n" +
+                " |_|  |_|      ";
+    }
+
+    @Override
+    public String getI() {
+        return "  _____        \n" +
+                " |_   _|       \n" +
+                "   | |         \n" +
+                "   | |         \n" +
+                "  _| |_        \n" +
+                " |_____|       ";
+    }
+
+    @Override
+    public String getJ() {
+        return "       _       \n" +
+                "      | |      \n" +
+                "      | |      \n" +
+                "  _   | |      \n" +
+                " | |__| |      \n" +
+                "  \\____/       ";
+    }
+
+    @Override
+    public String getK() {
+        return "  _  __        \n" +
+                " | |/ /        \n" +
+                " | ' /         \n" +
+                " |  <          \n" +
+                " | . \\         \n" +
+                " |_|\\_\\        ";
+    }
+
+    @Override
+    public String getL() {
+        return "  _            \n" +
+                " | |           \n" +
+                " | |           \n" +
+                " | |           \n" +
+                " | |____       \n" +
+                " |______|      ";
+    }
+
+    @Override
+    public String getM() {
+        return "  __  __       \n" +
+                " |  \\/  |      \n" +
+                " | \\  / |      \n" +
+                " | |\\/| |      \n" +
+                " | |  | |      \n" +
+                " |_|  |_|      ";
+    }
+
+    @Override
+    public String getN() {
+        return "  _   _        \n" +
+                " | \\ | |       \n" +
+                " |  \\| |       \n" +
+                " | . ` |       \n" +
+                " | |\\  |       \n" +
+                " |_| \\_|       ";
+    }
+
+    @Override
+    public String getO() {
+        return "   ____        \n" +
+                "  / __ \\       \n" +
+                " | |  | |      \n" +
+                " | |  | |      \n" +
+                " | |__| |      \n" +
+                "  \\____/       ";
+    }
+
+    @Override
+    public String getP() {
+        return "  _____        \n" +
+                " |  __ \\       \n" +
+                " | |__) |      \n" +
+                " |  ___/       \n" +
+                " | |           \n" +
+                " |_|           ";
+    }
+
+    @Override
+    public String getQ() {
+        return "   ____        \n" +
+                "  / __ \\       \n" +
+                " | |  | |      \n" +
+                " | |  | |      \n" +
+                " | |__| |      \n" +
+                "  \\___\\_\\      ";
+    }
+
+    @Override
+    public String getR() {
+        return "  _____        \n" +
+                " |  __ \\       \n" +
+                " | |__) |      \n" +
+                " |  _  /       \n" +
+                " | | \\ \\       \n" +
+                " |_|  \\_\\      ";
+    }
+
+    @Override
+    public String getS() {
+        return "   _____       \n" +
+                "  / ____|      \n" +
+                " | (___        \n" +
+                "  \\___ \\       \n" +
+                "  ____) |      \n" +
+                " |_____/       ";
+    }
+
+    @Override
+    public String getT() {
+        return "  _______      \n" +
+                " |__   __|     \n" +
+                "    | |        \n" +
+                "    | |        \n" +
+                "    | |        \n" +
+                "    |_|        ";
+    }
+
+    @Override
+    public String getU() {
+        return "  _    _       \n" +
+                " | |  | |      \n" +
+                " | |  | |      \n" +
+                " | |  | |      \n" +
+                " | |__| |      \n" +
+                "  \\____/       ";
+    }
+
+    @Override
+    public String getV() {
+        return " __      __    \n" +
+                " \\ \\    / /    \n" +
+                "  \\ \\  / /     \n" +
+                "   \\ \\/ /      \n" +
+                "    \\  /       \n" +
+                "     \\/        ";
+    }
+
+    @Override
+    public String getW() {
+        return " __          __\n" +
+                " \\ \\        / /\n" +
+                "  \\ \\  /\\  / / \n" +
+                "   \\ \\/  \\/ /  \n" +
+                "    \\  /\\  /   \n" +
+                "     \\/  \\/    ";
+    }
+
+    @Override
+    public String getX() {
+        return " __   __       \n" +
+                " \\ \\ / /       \n" +
+                "  \\ V /        \n" +
+                "   > <         \n" +
+                "  / . \\        \n" +
+                " /_/ \\_\\       ";
+    }
+
+    @Override
+    public String getY() {
+        return " __     __     \n" +
+                " \\ \\   / /     \n" +
+                "  \\ \\_/ /      \n" +
+                "   \\   /       \n" +
+                "    | |        \n" +
+                "    |_|        ";
+    }
+
+    @Override
+    public String getZ() {
+        return "  ______       \n" +
+                " |___  /       \n" +
+                "    / /        \n" +
+                "   / /         \n" +
+                "  / /__        \n" +
+                " /_____|       ";
+    }
+
+    @Override
+    public String get0() {
+        return "   ___   \n" +
+                "  / _ \\  \n" +
+                " | | | | \n" +
+                " | | | | \n" +
+                " | |_| | \n" +
+                "  \\___/  ";
+    }
+
+    @Override
+    public String get1() {
+        return "  __     \n" +
+                " /_ |    \n" +
+                "  | |    \n" +
+                "  | |    \n" +
+                "  | |    \n" +
+                "  |_|    ";
+    }
+
+    @Override
+    public String get2() {
+        return "  ___    \n" +
+                " |__ \\   \n" +
+                "    ) |  \n" +
+                "   / /   \n" +
+                "  / /_   \n" +
+                " |____|  ";
+    }
+
+    @Override
+    public String get3() {
+        return "  ____   \n" +
+                " |___ \\  \n" +
+                "   __) | \n" +
+                "  |__ <  \n" +
+                "  ___) | \n" +
+                " |____/  ";
+    }
+
+    @Override
+    public String get4() {
+        return "  _  _   \n" +
+                " | || |  \n" +
+                " | || |_ \n" +
+                " |__   _|\n" +
+                "    | |  \n" +
+                "    |_|  ";
+    }
+
+    @Override
+    public String get5() {
+        return "  _____  \n" +
+                " | ____| \n" +
+                " | |__   \n" +
+                " |___ \\  \n" +
+                "  ___) | \n" +
+                " |____/  ";
+    }
+
+    @Override
+    public String get6() {
+        return "    __   \n" +
+                "   / /   \n" +
+                "  / /_   \n" +
+                " | '_ \\  \n" +
+                " | (_) | \n" +
+                "  \\___/  ";
+    }
+
+    @Override
+    public String get7() {
+        return "  ______ \n" +
+                " |____  |\n" +
+                "     / / \n" +
+                "    / /  \n" +
+                "   / /   \n" +
+                "  /_/    ";
+    }
+
+    @Override
+    public String get8() {
+        return "   ___  \n" +
+                "  / _ \\ \n" +
+                " | (_) |\n" +
+                "  > _ < \n" +
+                " | (_) |\n" +
+                "  \\___/ ";
+    }
+
+    @Override
+    public String get9() {
+        return "   ___  \n" +
+                "  / _ \\ \n" +
+                " | (_) |\n" +
+                "  \\__, |\n" +
+                "    / / \n" +
+                "   /_/  ";
+    }
+
+    @Override
+    public String getSPACE() {
+        return "        \n" +
+                "        \n" +
+                "        \n" +
+                "        \n" +
+                "        \n" +
+                "        ";
+    }
+}
