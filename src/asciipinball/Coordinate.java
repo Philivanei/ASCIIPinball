3269ee8d54e0714b3bd167ac7bb079533b006ab0
Philipp Muhr <p.m33@gmx.de>
2019-06-11 17:08:09 +0200

    add JavaDoc to Coordinate

--- src/asciipinball/Coordinate.java
+++ src/asciipinball/Coordinate.java
@@ -1,18 +1,37 @@
 package asciipinball;
 
+/**
+ * Eine Koordinate
+ */
 public class Coordinate {
     private float x;
     private float y;
 
-    public Coordinate(float x, float y){
+    /**
+     * Erzeugt eine Koordinate.
+     *
+     * @param x x-Koordinate
+     * @param y y-Koordinate
+     */
+    public Coordinate(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Gibt die x-Koordinate zurück.
+     *
+     * @return x-Koordinate
+     */
     public float getX() {
         return x;
     }
 
+    /**
+     * Gibt die y-Koordinate zurück
+     *
+     * @return y-Koordinate
+     */
     public float getY() {
         return y;
     }
