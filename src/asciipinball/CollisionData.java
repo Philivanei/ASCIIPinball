2f69643fca5eeef2c3312037348df41844fef0ea
Michael Mican <michael.mican@stud.th-deg.de>
2019-06-14 14:51:23 +0200

    Edit JavaDoc of CollisionData

--- src/asciipinball/CollisionData.java
+++ src/asciipinball/CollisionData.java
@@ -2,12 +2,22 @@ package asciipinball;
 
 import asciipinball.shapes.Shapes;
 
+/**
+ * Speichert Kollisions daten
+ */
 public class CollisionData implements Comparable<CollisionData> {
     private Shapes collisionShape;
     private float collisionX;
     private float collisionY;
     private float distanceToBall;
 
+    /**
+     * Erstellt einen neuen KollisionsDaten Block
+     * @param collisionX X Koordinate der Kollision
+     * @param collisionY Y Koordinate der Kollision
+     * @param collisionShape Entity mit der Kollidiert wurde
+     * @param distanceToBall Abstand vom Kollisionspunkt zum Ball
+     */
     public CollisionData(float collisionX, float collisionY, Shapes collisionShape, float distanceToBall) {
         this.collisionShape = collisionShape;
         this.collisionX = collisionX;
@@ -15,30 +25,44 @@ public class CollisionData implements Comparable<CollisionData> {
         this.distanceToBall = distanceToBall;
     }
 
-    public CollisionData(float collisionX, float collisionY, Shapes collisionShape, float distanceToBall, float distanceToLinePointA, float distanceToLinePointB) {
-        this.collisionShape = collisionShape;
-        this.collisionX = collisionX;
-        this.collisionY = collisionY;
-        this.distanceToBall = distanceToBall;
-    }
-
+    /**
+     * Vergleicht die Abstände zwischen Ball und Objekt der Objekte
+     * @param o objekt mit dem verglichen wird
+     * @return größer/kleiner/gleich codiert als int (1/-1/0)
+     */
     @Override
     public int compareTo(CollisionData o) {
         return Float.compare(distanceToBall, o.distanceToBall);
     }
 
+    /**
+     * Gibt die Kollision Shape zurück
+     * @return Shape mit der Kolliert wurde
+     */
     public Shapes getCollisionShape() {
         return collisionShape;
     }
 
+    /**
+     * Gibt die X Koordinate des Kollisionspunktes zurück
+     * @return X Koordinate des Kollisionspunktes
+     */
     public float getCollisionX() {
         return collisionX;
     }
 
+    /**
+     * Gibt die Y Koordinate des Kollisionspunktes zurück
+     * @return Y Koordinate des Kollisionspunktes
+     */
     public float getCollisionY() {
         return collisionY;
     }
 
+    /**
+     * Gibt den Abstand zwischen Ball und Kollisionspunkt zurück
+     * @return Abstand zwischen Balll und Kollisionspunkt
+     */
     public float getDistanceToBall() {
         return distanceToBall;
     }
