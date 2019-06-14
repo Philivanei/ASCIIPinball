cfc321145154bce98e291c679938eb65e7e009df
Michael Mican <michael.mican@stud.th-deg.de>
2019-06-14 14:16:26 +0200

    Add JavaDoc for Exceptions

--- src/asciipinball/exceptions/ClassNotSupportedException.java
+++ src/asciipinball/exceptions/ClassNotSupportedException.java
@@ -1,11 +1,19 @@
 package asciipinball.exceptions;
 
+/**
+ * Exception für nicht implementierte Klassen
+ */
 public class ClassNotSupportedException extends RuntimeException {
 
-
+    /**
+     * Erstellt eine neue ClassNotSupportedException
+     */
     public ClassNotSupportedException() {
     }
 
+    /**
+     * Erstellt eine neue ClassNotSupportedException mit Nachricht
+     */
     public ClassNotSupportedException(String message){
         super(message);
     }
--- src/asciipinball/exceptions/NotSupportedNumberOfPlayersException.java
+++ src/asciipinball/exceptions/NotSupportedNumberOfPlayersException.java
@@ -1,11 +1,21 @@
 package asciipinball.exceptions;
 
+/**
+ * Exception für eine nicht unterstützte Spieleranzahll
+ */
 public class NotSupportedNumberOfPlayersException extends RuntimeException {
+
+    /**
+     * Erstellt eine NotSupportedNumberOfPlayersException
+     */
     public NotSupportedNumberOfPlayersException() {
+
     }
 
 
-
+    /**
+     * Erstellt eine NotSupportedNumberOfPlayersException mit Nachricht
+     */
     public NotSupportedNumberOfPlayersException(String message) {
         super(message);
     }
