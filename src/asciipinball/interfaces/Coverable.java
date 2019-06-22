a3e699a399f8908b70380eb0deab1bd21d955d50
Michael Mican <michael.mican@stud.th-deg.de>
2019-06-22 12:56:34 +0200

    Fix spelling in Drawable

--- src/asciipinball/interfaces/Coverable.java
+++ src/asciipinball/interfaces/Coverable.java
@@ -3,7 +3,7 @@ package asciipinball.interfaces;
 import asciipinball.shapes.Line;
 
 /**
- * Bei Coverable PhysicEntitys werden die Enden der Linien mit JointCovern versehen um ein aufspießen des Balls zu verhindern
+ * Bei Coverable PhysicEntitys werden die Enden der Linien mit JointCovern versehen um ein aufspießen des Balls zu verhindern.
  * Darf nur von nicht beweglichen Polygon Entities implementiert werden
  */
 public interface Coverable {
