b5884e0171b10f2b69b3a9239be2885331201779
Michael Mican <michael.mican@stud.th-deg.de>
2019-06-22 12:21:28 +0200

    Fix spelling in ControlsDisplay

--- src/asciipinball/graphics/ControlsDisplay.java
+++ src/asciipinball/graphics/ControlsDisplay.java
@@ -1,6 +1,21 @@
 package asciipinball.graphics;
 
+/**
+ * Erklärung der Steuerung am rechten unteren Bildschirmrand
+ */
 public class ControlsDisplay {
+
+    /**
+     * Erstellt ein neues ControlsDisplay
+     */
+    public ControlsDisplay(){
+
+    }
+
+    /**
+     * Gibt die Steuerungserklärung als String zurück
+     * @return Steuerungserklärung
+     */
     public String getString(){
         return "Steuerung:\n\n" +
                 "Alt     L Finger \n" +
