c1c1581311f8624e100ecfce0263f58859000169
Philipp Muhr <p.m33@gmx.de>
2019-06-22 13:44:32 +0200

    fix spelling in NonPhysicEntity

--- src/asciipinball/objects/nophysicsobject/NonPhysicEntity.java
+++ src/asciipinball/objects/nophysicsobject/NonPhysicEntity.java
@@ -6,24 +6,26 @@ import asciipinball.shapes.Circle;
 import asciipinball.sounds.Aui;
 
 /**
- * Abstrakte Oberklasse für Entities die über keine Kollisionsabfrage verfügen
+ * Abstrakte Oberklasse für Entities, die über keine Kollisionsabfrage verfügen
  */
 public abstract class NonPhysicEntity implements Drawable {
 
     protected Circle[] circles;
 
     /**
-     * Updated das Entity
+     * Aktualisiert das Entity
+     *
      * @param ball Ball mit dem interagiert werden soll
-     * @param aui Audio Inteface des Spiels
+     * @param aui  Audio Inteface des Spiels
      */
     public abstract void updateEntity(Ball ball, Aui aui);
 
     /**
      * Gibt alle Kreise zurück aus der die Entity besteht
+     *
      * @return Alle Kreise aus der die Entity besteht
      */
-    public Circle[] getCircles(){
+    public Circle[] getCircles() {
         return circles;
     }
 
