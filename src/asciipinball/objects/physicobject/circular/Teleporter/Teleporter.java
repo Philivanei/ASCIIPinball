c7cbd2ebcbc088e24ad47fb4e1ffe299fced1ffa
Philipp Muhr <p.m33@gmx.de>
2019-06-22 14:13:35 +0200

    fix spelling in Teleporter; fix spelling in TeleporterFactory

--- src/asciipinball/objects/physicobject/circular/Teleporter/Teleporter.java
+++ src/asciipinball/objects/physicobject/circular/Teleporter/Teleporter.java
@@ -7,6 +7,9 @@ import asciipinball.objects.physicobject.circular.CircleEntity;
 import asciipinball.playersandscore.PlayerManager;
 import asciipinball.shapes.Circle;
 
+/**
+ * Teleporter, der den Ball an den verbundenen Teleporter teleportiert
+ */
 public class Teleporter extends CircleEntity {
 
     private float x;
@@ -15,7 +18,8 @@ public class Teleporter extends CircleEntity {
     private static long deadTimeStart = 0; //Has to be static so all instances share the same timer
 
     //package private so only TeleporterFactory can instantiate a new Teleporter
-    Teleporter(PlayerManager playerManager, Coordinate coordinate){
+
+    Teleporter(PlayerManager playerManager, Coordinate coordinate) {
         super(playerManager);
         x = coordinate.getX();
         y = coordinate.getY();
@@ -23,22 +27,33 @@ public class Teleporter extends CircleEntity {
         circles[0] = new Circle(coordinate, 2f);
     }
 
+    /**
+     * Gibt die x-Koordinate des Teleporters zurück
+     *
+     * @return x-Koordinate
+     */
     public float getX() {
         return x;
     }
 
+    /**
+     * Gibt die y-Koordinate des Teleporters zurück
+     *
+     * @return y-Koordinate
+     */
     public float getY() {
         return y;
     }
 
     //Package private so only the TeleporterFactory can addLinks
-    void addLink(Teleporter teleporter){
+
+    void addLink(Teleporter teleporter) {
         destinationTeleporter = teleporter;
     }
 
     @Override
     protected Ball interactWithBall(Ball ball) {
-        if((System.currentTimeMillis() - deadTimeStart) > Settings.TELEPORTER_COOL_DOWN){
+        if ((System.currentTimeMillis() - deadTimeStart) > Settings.TELEPORTER_COOL_DOWN) {
             deadTimeStart = System.currentTimeMillis();
             playerManager.getCurrentPlayer().addToScore(1);
             return new Ball(destinationTeleporter.getX(), destinationTeleporter.getY(), ball.getRadius(), ball.getDirection(), ball.getVelocity());
