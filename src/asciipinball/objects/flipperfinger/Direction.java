691097a53bb56835eb873647674b415e1f210a13
Michael Mican <michael.mican@stud.th-deg.de>
2019-06-22 13:04:10 +0200

    Fix spelling in Enums

--- src/asciipinball/objects/flipperfinger/Direction.java
+++ src/asciipinball/objects/flipperfinger/Direction.java
@@ -1,6 +1,15 @@
 package asciipinball.objects.flipperfinger;
 
+/**
+ * Richtung der Bewegung der Fipperfinger
+ */
 public enum Direction {
+    /**
+     * Flipperfinger ist auf dem Weg nach oben
+     */
     UP,
+    /**
+     * Flipperfinger ist auf dem Weg nach unten
+     */
     DOWN
 }
