15792d066abee86436975c4a5ed1bbe5020f195e
Michael Mican <michael.mican@stud.th-deg.de>
2019-06-23 14:11:53 +0200

    correct controls info

--- src/asciipinball/graphics/ControlsDisplay.java
+++ src/asciipinball/graphics/ControlsDisplay.java
@@ -18,9 +18,9 @@ public class ControlsDisplay {
      */
     public String getString(){
         return "Steuerung:\n\n" +
-                "Alt     L Finger \n" +
-                "NUM0 R Finger\n" +
-                "Leer  Abschuss\n" +
-                "R          Reset";
+                "Left    L Finger \n" +
+                "Right R Finger\n" +
+                "Leer   Abschuss\n" +
+                "R           Reset";
     }
 }
